e78492cdc911f105717c14b35da19985ad9a565a
kidakai12 <dzehox@gmail.com>
2022-01-06 20:07:10 +0700

    Update Xem Giay Chung Nhan - HocVT

--- QuanLyTrungTamNgoaiNgu/WebForm/Pages/Shared/DanhSachPhongThiThiSinh.cshtml.cs
+++ QuanLyTrungTamNgoaiNgu/WebForm/Pages/Shared/DanhSachPhongThiThiSinh.cshtml.cs
@@ -20,6 +20,18 @@ namespace WebForm.Pages.Shared
         public string khoa { get; set; }
         [BindProperty]
         public string phong { get; set; }
+        [BindProperty]
+        public string nghe { get; set; }
+        [BindProperty]
+        public string noi { get; set; }
+        [BindProperty]
+        public string doc { get; set; }
+        [BindProperty]
+        public string  viet { get; set; }
+        [BindProperty]
+        public string ten { get; set; }
+        [BindProperty]
+        public string chungchi { get; set; }
         public void OnGet()
         {
             listkhoa = new KhoaThiBUS().List();
@@ -40,6 +52,17 @@ namespace WebForm.Pages.Shared
             HttpContext.Session.SetString("khoaphongthi", khoa);
             HttpContext.Session.SetString("maphongthi", phong);
             return Redirect("/Shared/DanhSachPhongThiThiSinh");
+        }
+        public IActionResult OnPostChungchi()
+        {
+            HttpContext.Session.SetString("xemchungchi", "true");
+            HttpContext.Session.SetString("nghe", nghe);
+            HttpContext.Session.SetString("noi", noi);
+            HttpContext.Session.SetString("doc", doc);
+            HttpContext.Session.SetString("viet", viet);
+            HttpContext.Session.SetString("ten", ten);
+            HttpContext.Session.SetString("chungchi", chungchi);
+            return Redirect("/Shared/XemGiayChungNhan");
         }
     }
 }
