462a948de9e7b90dc42ef4d1f3c495fdbd086e7f
thuydoanh <truongthuydoanh99@gmail.com>
2022-01-06 19:54:27 +0700

    Update KetQuaDiemThi - Doanh

--- QuanLyTrungTamNgoaiNgu/WebForm/Pages/Shared/KetQuaDiemThi.cshtml.cs
+++ QuanLyTrungTamNgoaiNgu/WebForm/Pages/Shared/KetQuaDiemThi.cshtml.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using DTO;
+using Microsoft.AspNetCore.Http;
 namespace WebForm.Pages.Shared
 {
     public class KetQuaDiemThiModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public KetQuaThiDTO ketquathi { set; get; }
+        [BindProperty]
+        public List<KhoaThiDTO> listKhoaThi { set; get; }
+        [BindProperty]
+        public string searchHoTen { set; get; }
+
+        [BindProperty]
+        public string searchvalue { set; get; }
+
+        public IActionResult OnPostSearch()
         {
+            HttpContext.Session.SetString("searchgiaychungnhan", searchvalue);
+            return Page();
         }
+
+        public IActionResult OnPostGiaychungnhan()
+        {
+            HttpContext.Session.SetString("sbd", ketquathi.SoBaoDanh);
+            HttpContext.Session.SetString("ngaythi", ketquathi.NgayThi);
+            return RedirectToPage("/Shared/XemGiayChungNhan");
+        
+        }
+
     }
 }
