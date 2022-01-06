7761b968ef90146b7b072e213d00a0cff77ee8e2
QuocHung <gamekingdom234@gmail.com>
2022-01-06 15:46:02 +0700

    GhiDiem - hung

--- QuanLyTrungTamNgoaiNgu/QuanLyTrungTamNgoaiNgu/GhiDiem.Designer.cs
+++ QuanLyTrungTamNgoaiNgu/QuanLyTrungTamNgoaiNgu/GhiDiem.Designer.cs
@@ -29,20 +29,273 @@ namespace QuanLyTrungTamNgoaiNgu
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.cbSearch = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cb_trinhdo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cb_khoathi = new System.Windows.Forms.ComboBox();
+            this.btnChiTiet = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnTao = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.datagridview_qlPhongThi = new System.Windows.Forms.DataGridView();
+            this.IdPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel7.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridview_qlPhongThi)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 35);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Ghi Điểm";
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel7.Controls.Add(this.cbSearch);
+            this.panel7.Controls.Add(this.txtSearch);
+            this.panel7.Location = new System.Drawing.Point(503, 6);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(568, 62);
+            this.panel7.TabIndex = 9;
+            // 
+            // cbSearch
+            // 
+            this.cbSearch.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSearch.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbSearch.FormattingEnabled = true;
+            this.cbSearch.Location = new System.Drawing.Point(372, 12);
+            this.cbSearch.Name = "cbSearch";
+            this.cbSearch.Size = new System.Drawing.Size(189, 37);
+            this.cbSearch.TabIndex = 2;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(3, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(363, 36);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.cb_trinhdo);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.cb_khoathi);
+            this.panel1.Controls.Add(this.btnChiTiet);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.btnXoa);
+            this.panel1.Controls.Add(this.btnTao);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.datagridview_qlPhongThi);
+            this.panel1.Location = new System.Drawing.Point(10, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1061, 560);
+            this.panel1.TabIndex = 8;
+            // 
+            // cb_trinhdo
+            // 
+            this.cb_trinhdo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_trinhdo.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_trinhdo.FormattingEnabled = true;
+            this.cb_trinhdo.Location = new System.Drawing.Point(303, 19);
+            this.cb_trinhdo.Name = "cb_trinhdo";
+            this.cb_trinhdo.Size = new System.Drawing.Size(122, 37);
+            this.cb_trinhdo.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(220, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 22);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Trình độ";
+            // 
+            // cb_khoathi
+            // 
+            this.cb_khoathi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_khoathi.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_khoathi.FormattingEnabled = true;
+            this.cb_khoathi.Location = new System.Drawing.Point(75, 18);
+            this.cb_khoathi.Name = "cb_khoathi";
+            this.cb_khoathi.Size = new System.Drawing.Size(122, 37);
+            this.cb_khoathi.TabIndex = 3;
+            // 
+            // btnChiTiet
+            // 
+            this.btnChiTiet.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiTiet.Location = new System.Drawing.Point(814, 18);
+            this.btnChiTiet.Name = "btnChiTiet";
+            this.btnChiTiet.Size = new System.Drawing.Size(117, 48);
+            this.btnChiTiet.TabIndex = 13;
+            this.btnChiTiet.Text = "Chi tiết";
+            this.btnChiTiet.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(937, 18);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(117, 48);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "Reload";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(691, 18);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(117, 48);
+            this.btnXoa.TabIndex = 11;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnTao
+            // 
+            this.btnTao.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTao.Location = new System.Drawing.Point(516, 18);
+            this.btnTao.Name = "btnTao";
+            this.btnTao.Size = new System.Drawing.Size(169, 48);
+            this.btnTao.TabIndex = 9;
+            this.btnTao.Text = "Tạo Phiếu điểm";
+            this.btnTao.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 22);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Khóa";
+            // 
+            // datagridview_qlPhongThi
+            // 
+            this.datagridview_qlPhongThi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagridview_qlPhongThi.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdPhong,
+            this.Column1,
+            this.Column2,
+            this.Column5,
+            this.Column3,
+            this.Column4});
+            this.datagridview_qlPhongThi.Location = new System.Drawing.Point(6, 81);
+            this.datagridview_qlPhongThi.Name = "datagridview_qlPhongThi";
+            this.datagridview_qlPhongThi.RowHeadersWidth = 51;
+            this.datagridview_qlPhongThi.RowTemplate.Height = 24;
+            this.datagridview_qlPhongThi.Size = new System.Drawing.Size(1048, 473);
+            this.datagridview_qlPhongThi.TabIndex = 7;
+            // 
+            // IdPhong
+            // 
+            this.IdPhong.HeaderText = "Mã Phòng";
+            this.IdPhong.MinimumWidth = 8;
+            this.IdPhong.Name = "IdPhong";
+            this.IdPhong.Visible = false;
+            this.IdPhong.Width = 220;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Tên Phòng";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 220;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Mã Khóa thi";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Visible = false;
+            this.Column2.Width = 220;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Khóa Thi";
+            this.Column5.MinimumWidth = 8;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 220;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Trình độ";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 220;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Ca thi";
+            this.Column4.MinimumWidth = 8;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 222;
             // 
             // GhiDiem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1229, 776);
+            this.ClientSize = new System.Drawing.Size(1074, 640);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "GhiDiem";
             this.Text = "GhiDiem";
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridview_qlPhongThi)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.ComboBox cbSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cb_trinhdo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cb_khoathi;
+        private System.Windows.Forms.Button btnChiTiet;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnTao;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView datagridview_qlPhongThi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdPhong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
     }
 }
--- QuanLyTrungTamNgoaiNgu/QuanLyTrungTamNgoaiNgu/GhiDiem.cs
+++ QuanLyTrungTamNgoaiNgu/QuanLyTrungTamNgoaiNgu/GhiDiem.cs
@@ -7,14 +7,204 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BUS;
+using DTO;
+using QuanLyTourDuLich;
 
 namespace QuanLyTrungTamNgoaiNgu
 {
     public partial class GhiDiem : Form
     {
+        List<PhongThiDTO> list;
+        List<PhongThiDTO> listsearch;
+        List<KhoaThiDTO> listkhoa;
         public GhiDiem()
         {
             InitializeComponent();
+            list = new PhongThiBUS().ListAll();
+            listkhoa = new KhoaThiBUS().List();
+            listsearch = new List<PhongThiDTO>();
+            datagridview_qlPhongThi.AutoGenerateColumns = false;
+            List<string> listtype = new List<string> { "Tên Phòng", "Ca thi" };
+            cbSearch.DataSource = listtype;
+            cbSearch.SelectedIndex = 0;
+
+            //Lấy danh sách theo khóa và trình độ
+            foreach (KhoaThiDTO item in listkhoa)
+                cb_khoathi.Items.Add(item.Id_KhoaThi);
+            cb_khoathi.SelectedIndex = 0;
+            List<string> trinhdo = new List<string> { "A2", "B1" };
+            cb_trinhdo.DataSource = trinhdo;
+            cb_trinhdo.SelectedIndex = 0;
+            listbasekhoa();
+        }
+        public GhiDiem(string khoa)
+        {
+            InitializeComponent();
+            list = new PhongThiBUS().ListAll();
+            listkhoa = new KhoaThiBUS().List();
+            listsearch = new List<PhongThiDTO>();
+            datagridview_qlPhongThi.AutoGenerateColumns = false;
+            List<string> listtype = new List<string> { "Tên Phòng", "Ca thi" };
+            cbSearch.DataSource = listtype;
+            cbSearch.SelectedIndex = 0;
+
+            //Lấy danh sách theo khóa và trình độ
+            foreach (KhoaThiDTO item in listkhoa)
+                cb_khoathi.Items.Add(item.Id_KhoaThi);
+            cb_khoathi.SelectedItem = khoa;
+            List<string> trinhdo = new List<string> { "A2", "B1" };
+            cb_trinhdo.DataSource = trinhdo;
+            cb_trinhdo.SelectedIndex = 0;
+        }
+        public void BindGrid(List<PhongThiDTO> list)
+        {
+            datagridview_qlPhongThi.Rows.Clear();
+            datagridview_qlPhongThi.Refresh();
+            foreach (PhongThiDTO item in list)
+            {
+                datagridview_qlPhongThi.Rows.Add(
+                    item.Id_PhongThi,
+                    item.Ten_PhongThi,
+                    item.Id_KhoaThi,
+                    new PhongThiBUS().getTenKhoa(item.Id_KhoaThi),
+                    item.TrinhDo,
+                    item.CaThi);
+            }
+        }
+        public void Search()
+        {
+            string typesearch = cbSearch.SelectedItem.ToString();
+            string searchkey = txtSearch.Text;
+            List<PhongThiDTO> listsearch = new List<PhongThiDTO>();
+
+            switch (typesearch)
+            {
+                case "Tên Phòng":
+                    listsearch = list.FindAll(x => x.Ten_PhongThi.Contains(searchkey));
+                    break;
+                case "Ca thi":
+                    listsearch = list.FindAll(x => x.CaThi.Contains(searchkey));
+                    break;
+                default:
+                    break;
+            }
+            BindGrid(listsearch);
+        }
+
+        private void txtSearch_TextChanged(object sender, System.EventArgs e)
+        {
+            Search();
+        }
+
+        private void cbSearch_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            Search();
+        }
+
+        private void btnTao_Click(object sender, System.EventArgs e)
+        {
+            string idphongthi = new PhongThiBUS().MakeID();
+            string stt = "";
+            if (listsearch.Count == 0)
+                stt = "01";
+            if (listsearch.Count < 9)
+                stt = "0" + (listsearch.Count + 1);
+            else stt = (listsearch.Count + 1).ToString();
+            string trinhdo = cb_trinhdo.SelectedItem.ToString();
+            string tenphongthi = trinhdo + "P" + stt;
+            string khoathi = cb_khoathi.SelectedItem.ToString();
+            PhongThiDTO phongmoi = new PhongThiDTO(idphongthi, tenphongthi, khoathi, trinhdo, " ");
+            using (var form = new ThemPhongThi(list, phongmoi))
+            {
+                var result = form.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    list = form.list;
+                    BindGrid(list);
+                }
+            }
+            /*using (var form = new QuanLyPhongThi_Them(list,phongmoi))
+            {
+                var result = form.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    list = form.list;
+                    BindGrid(list);
+                }
+            }*/
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (datagridview_qlPhongThi.SelectedCells.Count > 0)
+            {
+                int selectedrowindex = datagridview_qlPhongThi.SelectedCells[0].RowIndex;
+                DataGridViewRow selectedRow = datagridview_qlPhongThi.Rows[selectedrowindex];
+                string cellValue = Convert.ToString(selectedRow.Cells["IdPhong"].Value);
+
+                DialogResult dialogResult = MessageBox.Show("Thao tác này sẽ xóa phòng và toàn bộ thí sinh trong phòng (nếu có).", "Bạn chắc chắn muốn xóa?", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    if (new PhongThiBUS().Delete(cellValue))
+                    {
+                        MessageBox.Show("Xóa thành công");
+                        list.RemoveAll(x => x.Id_PhongThi.Equals(cellValue));
+                        BindGrid(list);
+                    }
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    return;
+                }
+
+
+            }
+        }
+
+        private void btnChiTiet_Click(object sender, EventArgs e)
+        {
+            if (datagridview_qlPhongThi.SelectedCells.Count > 0)
+            {
+                int selectedrowindex = datagridview_qlPhongThi.SelectedCells[0].RowIndex;
+                DataGridViewRow selectedRow = datagridview_qlPhongThi.Rows[selectedrowindex];
+                string cellValue = Convert.ToString(selectedRow.Cells["IdPhong"].Value);
+                int s = 0;
+
+                using (var form = new ChiTietPhongThi(list.Find(x => x.Id_PhongThi.Equals(cellValue))))
+                {
+                    var result = form.ShowDialog();
+                    BindGrid(list);
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtSearch.Text = "";
+            cbSearch.SelectedIndex = 1;
+            BindGrid(list);
+        }
+        private void listbasekhoa()
+        {
+            try
+            {
+                if (cb_khoathi.SelectedItem != null && cb_trinhdo.SelectedItem != null)
+                    listsearch = list.FindAll(x =>
+                                x.Id_KhoaThi == cb_khoathi.SelectedItem.ToString() &&
+                                x.TrinhDo == cb_trinhdo.SelectedItem.ToString());
+                BindGrid(listsearch);
+            }
+            catch (Exception e) { }
+        }
+        private void cb_khoathi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listbasekhoa();
+        }
+
+        private void cb_trinhdo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listbasekhoa();
         }
     }
 }
