6e7c4753bc9fcd1c7065954c11fb558c70898542
kidakai12 <dzehox@gmail.com>
2022-01-06 19:40:07 +0700

    Update XemDanhSach Thi - HocVT

--- QuanLyTrungTamNgoaiNgu/WebForm/Pages/Shared/DanhSachPhongThiThiSinh.cshtml.cs
+++ QuanLyTrungTamNgoaiNgu/WebForm/Pages/Shared/DanhSachPhongThiThiSinh.cshtml.cs
@@ -4,13 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using BUS;
+using DTO;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 namespace WebForm.Pages.Shared
 {
     public class DanhSachPhongThiThiSinhModel : PageModel
     {
+        [BindProperty]
+        public List<KhoaThiDTO> listkhoa { get; set; }
+        [BindProperty]
+        public List<PhongThiDTO> listphong { get; set; }
+        [BindProperty]
+        public string khoa { get; set; }
+        [BindProperty]
+        public string phong { get; set; }
         public void OnGet()
         {
+            listkhoa = new KhoaThiBUS().List();
+            if (HttpContext.Session.GetString("layphongthi") == "true")
+            {
+                khoa = HttpContext.Session.GetString("khoaphongthi");
+            }
+        }
+        public IActionResult OnPostGetting()
+        {
+                HttpContext.Session.SetString("layphongthi", "true");
+            HttpContext.Session.SetString("khoaphongthi", khoa);
+            return Redirect("/Shared/DanhSachPhongThiThiSinh");
+        }
+        public IActionResult OnPostList()
+        {
+            HttpContext.Session.SetString("laydanhsach", "true");
+            HttpContext.Session.SetString("khoaphongthi", khoa);
+            HttpContext.Session.SetString("maphongthi", phong);
+            return Redirect("/Shared/DanhSachPhongThiThiSinh");
         }
     }
 }
--- QuanLyTrungTamNgoaiNgu/WebForm/Pages/Shared/ThiSinhDangKy.cshtml.cs
+++ QuanLyTrungTamNgoaiNgu/WebForm/Pages/Shared/ThiSinhDangKy.cshtml.cs
@@ -22,18 +22,39 @@ namespace WebForm.Pages.Shared
 
         public IActionResult OnPost()
         {
-            if (new ThiSinhBUS().Insert(new ThiSinhDTO(dangky.Cccd_TS, dangky.HoTen_TS, dangky.GioiTinh_TS, dangky.NgaySinh_TS,
-                dangky.NoiSinh_TS, dangky.NgayCap_TS, dangky.NoiCap_TS, dangky.Sdt_TS, dangky.Email_TS)))
+            dangky2.Cccd_TS = dangky.Cccd_TS;
+            dangky2.LePhiThi = "500,000 VND";
+            if (new ThiSinhBUS().Exists(dangky.Cccd_TS))
             {
-                HttpContext.Session.SetString("themdangky", "true");
+                if (new DangKyBUS().Insert(dangky2))
+                {
+                    HttpContext.Session.SetString("themdangky", "true");
+                    return Redirect("/Shared/ThiSinhDangKy");
+                }
+                else
+                {
+                    HttpContext.Session.SetString("themdangky", "false");
+                    return Redirect("/Shared/ThiSinhDangKy");
+                }
+            }
+            else if (new ThiSinhBUS().Insert(dangky))
+            {
+                if (new DangKyBUS().Insert(dangky2))
+                {
+                    HttpContext.Session.SetString("themdangky", "true");
+                    return Redirect("/Shared/ThiSinhDangKy");
+                }
+                else
+                {
+                    HttpContext.Session.SetString("themdangky", "false");
+                    return Redirect("/Shared/ThiSinhDangKy");
+                }
+            }
+            else
+            {
+                HttpContext.Session.SetString("themdangky", "false");
                 return Redirect("/Shared/ThiSinhDangKy");
             }
-            HttpContext.Session.SetString("themdangky", "false");
-            return Redirect("/Shared/ThiSinhDangKy");
-
-            //dangky2.Cccd_TS = dangky.Cccd_TS;
-            //dangky2.LePhiThi = "500,000 VND";
-            //return Page();
         }
     }
 }
